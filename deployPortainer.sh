dc1fdcabc676b9bc5645e72a55f0f2ae42e0dd40
kameshX007 <112748572+kameshX007@users.noreply.github.com>
2022-09-03 18:51:17 +0530

    Update and rename deployPortainer.sh to  deployWatchtower.sh

--- deployPortainer.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-echo "Checking if Portainer already deployed...">deployPortainer.log;
-container=$(docker ps -q --filter ancestor=portainer )>>deployPortainer.log;
-[ -z "$container" ] && echo "No running container found">>deployPortainer.log || docker rm -f $container;
-echo "Deploying Portainer...">>deployPortainer.log;
-sudo docker run -d -p 9000:9000 --name=portainer  --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v /home/ubuntu/docker/portainer/data:/data portainer/portainer-ce;
-echo "Portainer deployment successfull...">>deployPortainer.log;
-exit 0
---
+++ deployWatchtower.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+echo "Checking if Watchtower already deployed...">deployWatchtower.log;
+container=$(docker ps -q --filter ancestor=watchtower )>>deployWatchtower.log;
+[ -z "$container" ] && echo "No running container found">>deployWatchtower.log || docker rm -f $container;
+echo "Deploying Watchtower...">>deployWatchtower.log;
+sudo docker run -d --name=watchtower --restart=always -v /var/run/docker.sock:/var/run/docker.sock containrrr/watchtower --debug --cleanup --schedule "0 0 1 * * *"
+echo "Watchtower deployment successfull...">>deployWatchtower.log;
+exit 0
