734b58f489ae9ca2e4f72b93c453fcdf180460e8
kameshX007 <112748572+kameshX007@users.noreply.github.com>
2022-09-04 13:23:22 +0530

    Update deployWatchtower.sh

--- deployWatchtower.sh
+++ deployWatchtower.sh
@@ -7,10 +7,10 @@ container=$(docker ps -q --filter ancestor=containrrr/watchtower )>>deployWatcht
 #Checking whether username is passed to script
 #A docker folder will be created inside this users home directory and container persistant data will be stored there
 dockerUser=$1;
-[ -z "$dockerUser" ] && echo "Please pass username...">>deployWatchtower.log && exit 0 || echo "Deploying for user $dockerUser">>deployWatchtower.log;
+[ -z "$dockerUser" ] && echo "###Please pass username...!!!">>deployWatchtower.log && exit 0 || echo "Deploying for user $dockerUser">>deployWatchtower.log;
 echo "Deploying Watchtower for user $dockerUser...">>deployWatchtower.log;
 
 #Deploying container
 sudo docker run -d --name=watchtower --restart=always -v /var/run/docker.sock:/var/run/docker.sock containrrr/watchtower --debug --cleanup --schedule "0 0 1 * * *"
-echo "Watchtower deployment successfull...">>deployWatchtower.log;
+echo "###Watchtower deployment successfull...">>deployWatchtower.log;
 exit 0
