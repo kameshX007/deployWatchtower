c1b557dc43c19e2b24a62ea26361929ebfecc769
kameshX007 <kameshbook@gmail.com>
2022-09-25 13:33:50 +0530

    #

--- deployWatchtower.sh
+++ deployWatchtower.sh
@@ -1,15 +1,16 @@
 #!/bin/sh
-#Checking & Removing existing container
-echo "Checking if Watchtower already deployed...">deployWatchtower.log;
-container=$(docker ps -q --filter ancestor=containrrr/watchtower )>>deployWatchtower.log;
-[ -z "$container" ] && echo "No running container found">>deployWatchtower.log || docker rm -f $container;
-
 #Checking whether username is passed to script
 #A docker folder will be created inside this users home directory and container persistant data will be stored there
 dockerUser=$1;
-[ -z "$dockerUser" ] && echo "###Please pass username...!!!">>deployWatchtower.log && exit 0 || echo "Deploying Watchtower for user $dockerUser">>deployWatchtower.log;
+[ -z "$dockerUser" ] && echo "Please pass username...!!!" && exit 0 || echo "Deploying Watchtower for user $dockerUser";
+
+#Checking & Removing existing container
+echo "Checking if Watchtower already deployed...";
+container=$(docker ps -q --filter ancestor=containrrr/watchtower );
+echo $container;
+[ -z "$container" ] && echo "No running container found" || docker rm -f $container;
 
 #Deploying container
-docker run -d --name=watchtower --restart=always -v /var/run/docker.sock:/var/run/docker.sock containrrr/watchtower --debug --cleanup --schedule "0 0 1 * * *"
-echo "###Watchtower deployment successfull...">>deployWatchtower.log;
+docker run -d --name=watchtower --network tunnel --restart=always -v /var/run/docker.sock:/var/run/docker.sock containrrr/watchtower --debug --cleanup --schedule "@hourly"
+echo "Watchtower deployment successfull...";
 exit 0
